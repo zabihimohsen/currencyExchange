dc920d2882e4f77583ac36bddf986547ab49dd3b
Mohsen Zabihi <mohsen.zabihi10@gmail.com>
2022-03-12 01:38:56 +0330

    UI of main page, add hilt application for entry point, add retrofit library

---
+++ app/src/main/java/zabihi/mohsen/currencyexchanger/ExchangeApplication.kt
@@ -0,0 +1,8 @@
+package zabihi.mohsen.currencyexchanger
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ExchangeApplication :Application() {
+}
--- app/src/main/java/zabihi/mohsen/currencyexchanger/MainActivity.kt
+++ app/src/main/java/zabihi/mohsen/currencyexchanger/MainActivity.kt
@@ -2,10 +2,13 @@ package zabihi.mohsen.currencyexchanger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import zabihi.mohsen.currencyexchanger.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var  binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
