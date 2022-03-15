9a54eaa67896ab4bf77828d2e9aa6e8f00782674
Mohsen Zabihi <mohsen.zabihi10@gmail.com>
2022-03-15 21:44:11 +0330

    removed unused xml, changed api call interval

--- app/src/main/java/zabihi/mohsen/currencyexchanger/data/Constants.kt
+++ app/src/main/java/zabihi/mohsen/currencyexchanger/data/Constants.kt
@@ -8,7 +8,7 @@ object Constants {
     const val freeTradeNum = 5 // number of free trades each user will have
     const val tradeFee = 0.7 // amount of fee in percentage, applied after 5th trade of user
     const val totalTradeNum = "trades" // key used for saving total trades number of a user
-    const val apiCallInterval :Long = 30000 //time in milli seconds to call api again
+    const val apiCallInterval :Long = 5000 //time in milli seconds to call api again
     const val eurKey = "EUR"
     const val initBalanceAmount = 1000.0 // Amount of Euros each user will get by default at first install
     const val launchedBefore = "launchedBefore" // Key used for checking if app is launched before or no
